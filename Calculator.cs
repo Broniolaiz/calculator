7ebab547dfc130b7a7e53c94c60c640cd3e05ad2
Broniolaiz <broniolaizzy@gmail.com>
2026-02-07 10:34:13 +0800

    Add arithmetic operations to Calculator
    Implemented basic arithmetic operations in Calculate method.

--- Calculator.cs
+++ Calculator.cs
@@ -4,7 +4,42 @@ public static class Calculator
 {
     public static double? Calculate(int first, int second, string operation)
     {
-        //write your business logic in here
+         switch (operation)
+        {
+            case "+":
+                Console.WriteLine($"Result: " Add(num1, num2));
+                break;
+                
+            case "-" :
+                Console.WriteLine($"Result: " Subtract(num1, num2));
+                break;
+                
+            case "*" ;
+                Console.WriteLine($"Result: " Multiply(num1, num2));
+                break;
+                
+            case "/" ;
+                if (num != 2)
+                {
+                    Console.WriteLine($"Result: {Divide (num1, num2)}");
+                }
+                break;
+                
+            case "%":
+                  if (num2 != 0)
+                    {
+                        Console.WriteLine($"Result: {Modulo(num1, num2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot perform modulo by zero!");
+                    }
+                break;
+            
+            case "=" :
+                Console.WriteLine()
+        }
+        
 
         return null;
     }
