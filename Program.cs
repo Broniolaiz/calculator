a88c8c685131aa40e2c082b5ba6f67e7d496fbc7
Broniolaiz <broniolaizzy@gmail.com>
2026-02-07 10:32:34 +0800

    Refactor calculator program in Program.cs
    Fix syntax errors and improve calculator logic.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,73 @@
-﻿//write your console display in here
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while(operation) {
+            
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Console.ReadLine();
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Console.ReadLine();
+            Console.WriteLine("+,-,*,/,% :=");
+            operation = Console.ReadLine();
+        }
+            
+            Console.WriteLine("Incorrect Operation used, please try again");
+            
+            Console.WriteLine("Choose an operation");
+            Console.WriteLine("Addition (+)");
+            Console.WriteLine("Subtraction (-)");
+            Console.WriteLine("Multiplication (*)");
+            Console.WriteLine("Division (/)");
+            Console.WriteLine("Modulo (%)");
+            Console.WriteLine("Exit(=)");
+            
+            string operation Console.ReadLine();
+            
+            
+        }
+    }
+    
+    public static void Mian(String[] args)
+    {
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine($"Result: " Add(num1, num2));
+                break;
+                
+            case "-" :
+                Console.WriteLine($"Result: " Subtract(num1, num2));
+                break;
+                
+            case "*" ;
+                Console.WriteLine($"Result: " Multiply(num1, num2));
+                break;
+                
+            case "/" ;
+                if (num != 2)
+                {
+                    Console.WriteLine($"Result: {Divide (num1, num2)}");
+                }
+                break;
+                
+            case "%":
+                  if (num2 != 0)
+                    {
+                        Console.WriteLine($"Result: {Modulo(num1, num2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot perform modulo by zero!");
+                    }
+                break;
+            
+            case "=" :
+                Console.WriteLine()
+        }
+        
+    }
+}﻿//write your console display in here
