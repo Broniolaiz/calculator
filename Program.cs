fbb3ac8a387713ac62c9b928a467c8ec0270cbc1
Broniolaiz <broniolaizzy@gmail.com>
2026-02-07 10:37:52 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -31,43 +31,5 @@ public class Program
         }
     }
     
-    public static void Mian(String[] args)
-    {
-        switch (operation)
-        {
-            case "+":
-                Console.WriteLine($"Result: " Add(num1, num2));
-                break;
-                
-            case "-" :
-                Console.WriteLine($"Result: " Subtract(num1, num2));
-                break;
-                
-            case "*" ;
-                Console.WriteLine($"Result: " Multiply(num1, num2));
-                break;
-                
-            case "/" ;
-                if (num != 2)
-                {
-                    Console.WriteLine($"Result: {Divide (num1, num2)}");
-                }
-                break;
-                
-            case "%":
-                  if (num2 != 0)
-                    {
-                        Console.WriteLine($"Result: {Modulo(num1, num2)}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error: Cannot perform modulo by zero!");
-                    }
-                break;
-            
-            case "=" :
-                Console.WriteLine()
-        }
-        
-    }
-}﻿//write your console display in here
+    
+
